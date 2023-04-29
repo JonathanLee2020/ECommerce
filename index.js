6f1e3c83dc58965200797f81a4a8d045b6c5b293
Jonathan <jonathanlee882@gmail.com>
2023-04-29 19:30:20 +1000

    when backdrop is activated, we lose the option to scroll and cap the menu height to 100vh

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("hello gamers we're doing some gaming today")
+
+function openMenu() {
+    //adds class to body to let us know the menu is open
+    document.body.classList += " menu--open"
+}
+
+function closeMenu() {
+    document.body.classList.remove("menu--open")
+}
